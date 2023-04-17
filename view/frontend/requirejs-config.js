8c8676625e19d6c49f0102de37c674091057b48f
Eric Wright <eric.wright@algolia.com>
2023-04-17 10:15:22 -0400

    Demonstrate use of new Autocomplete v1 front end events.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -12,56 +12,51 @@
  *   2. Via a JavaScript mixin (recommended)
  */
 
-var config = {
-  map: {
-    "*": {
-      algoliaHooks: "Algolia_CustomAlgolia/hooks",
+const config = {
+    map: {
+        "*": {
+            // Include your own hooks to handle front end events - see https://www.algolia.com/doc/integration/magento-2/customize/custom-front-end-events/
+            algoliaHooks: 'Algolia_CustomAlgolia/hooks',
 
-      ////////////////////////////////////
-      // AUTOCOMPLETE TEMPLATE OVERRIDE //
-      ////////////////////////////////////
+            // Additional lib for recent searches plugin - OPTIONAL
+            algoliaRecentSearches: 'Algolia_CustomAlgolia/autocomplete-plugin-recent-searches'
 
-      // Uncomment the following line to override the products.js functional template
-      // productsHtml': 'Algolia_CustomAlgolia/internals/template/autocomplete/products'
+            ////////////////////////////////////
+            // AUTOCOMPLETE TEMPLATE OVERRIDE //
+            ////////////////////////////////////
+
+            // Uncomment the following line to override the products.js functional template
+            // productsHtml: 'Algolia_CustomAlgolia/internals/template/autocomplete/products'
+        },
     },
-  },
 
-  /////////////////////
-  // FRONT END HOOKS //
-  /////////////////////
+    ////////////////////////
+    // AUTOCOMPLETE MIXIN //
+    ////////////////////////
 
-  // Uncomment the following line to include front-end hook methods - see https://www.algolia.com/doc/integration/magento-2/customize/custom-front-end-events/
-  // deps: ["algoliaHooks"],
+    /**
+     *  You can add a mixin to customize only a single portion of a template based on the corresponding function.
+     *  Use the `html` tagged template literal supplied via the function arguments to return your custom content as HTML.
+     */
 
-  //,
-
-  ////////////////////////
-  // AUTOCOMPLETE MIXIN //
-  ////////////////////////
-
-  /**
-   *  You can add a mixin to customize only a single portion of a template based on the corresponding function.
-   *  Use the `html` tagged template literal supplied via the function arguments to return your custom content as HTML.
-   */
-
-  config: {
-    mixins: {
-      // Uncomment the following mixins to override the hit template via a JavaScript mixin for a given source
-      // "Algolia_AlgoliaSearch/internals/template/autocomplete/products": {
-      //   "Algolia_CustomAlgolia/template/autocomplete/products-mixin": true,
-      // },
-      // "Algolia_AlgoliaSearch/internals/template/autocomplete/categories": {
-      //   "Algolia_CustomAlgolia/template/autocomplete/categories-mixin": true,
-      // },
-      // "Algolia_AlgoliaSearch/internals/template/autocomplete/pages": {
-      //   "Algolia_CustomAlgolia/template/autocomplete/pages-mixin": true,
-      // },
-      // "Algolia_AlgoliaSearch/internals/template/autocomplete/additional-section": {
-      //   "Algolia_CustomAlgolia/template/autocomplete/additional-section-mixin": true,
-      // },
-      // "Algolia_AlgoliaSearch/internals/template/autocomplete/suggestions": {
-      //   "Algolia_CustomAlgolia/template/autocomplete/suggestions-mixin": true,
-      // },
+    config: {
+        mixins: {
+            // Uncomment the following mixins to override the hit template via a JavaScript mixin for a given source
+            // "Algolia_AlgoliaSearch/internals/template/autocomplete/products": {
+            //   "Algolia_CustomAlgolia/template/autocomplete/products-mixin": true,
+            // },
+            // "Algolia_AlgoliaSearch/internals/template/autocomplete/categories": {
+            //   "Algolia_CustomAlgolia/template/autocomplete/categories-mixin": true,
+            // },
+            // "Algolia_AlgoliaSearch/internals/template/autocomplete/pages": {
+            //   "Algolia_CustomAlgolia/template/autocomplete/pages-mixin": true,
+            // },
+            // "Algolia_AlgoliaSearch/internals/template/autocomplete/additional-section": {
+            //   "Algolia_CustomAlgolia/template/autocomplete/additional-section-mixin": true,
+            // },
+            // "Algolia_AlgoliaSearch/internals/template/autocomplete/suggestions": {
+            //   "Algolia_CustomAlgolia/template/autocomplete/suggestions-mixin": true,
+            // },
+        },
     },
-  },
 };
