57793f7c1dcc9beba0468c3a1ab6e80856ce1648
mohitalgolia <101385480+mohitalgolia@users.noreply.github.com>
2023-10-23 21:11:36 +0530

    Add files via upload

---
+++ Observer/UpdateProductData.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Algolia\CustomAlgolia\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class UpdateProductData implements ObserverInterface
+{
+    public function __construct()
+    {
+        // Observer initialization code...
+        // You can use dependency injection to get any class this observer may need.
+    }
+
+    public function execute(Observer $observer)
+    {
+        $productData = $observer->getData('productObject');
+        $this->logger(json_encode($productData->getData()));
+    }
+
+    public function logger($string)
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/customAlgoliaDebugger.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        echo $string; exit();
+        $logger->info('Custom Algolia Logger: '.$string);
+    }
+}
+
